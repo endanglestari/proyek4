5102caadf0beef6b311bc92f6f1b6296ca97cd82
endanglestari <71061298+endanglestari@users.noreply.github.com>
2020-11-14 18:34:39 +0700

    Penambahan CRUD pada Form Register

--- system/resources/views/register.blade.php
+++ system/resources/views/register.blade.php
@@ -1,92 +1,82 @@
-<!doctype html>
-<html class="no-js" lang="en">
-    <head>
-        <meta charset="utf-8">
-        <meta http-equiv="x-ua-compatible" content="ie=edge">
-        <title>Register Admin</title>
-        <meta name="description" content="">
-        <meta name="keywords" content="">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        
-        <link rel="icon" href="{{url('public')}}/smk.png" type="image/x-icon" />
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>Admin Vegefoods</title>
 
-        <link href="https://fonts.googleapis.com/css?family=Nunito+Sans:300,400,600,700,800" rel="stylesheet">
-        
-        <link rel="stylesheet" href="{{url('public')}}/plugins/bootstrap/dist/css/bootstrap.min.css">
-        <link rel="stylesheet" href="{{url('public')}}/plugins/fontawesome-free/css/all.min.css">
-        <link rel="stylesheet" href="{{url('public')}}/plugins/ionicons/dist/css/ionicons.min.css">
-        <link rel="stylesheet" href="{{url('public')}}/plugins/icon-kit/dist/css/iconkit.min.css">
-        <link rel="stylesheet" href="{{url('public')}}/plugins/perfect-scrollbar/css/perfect-scrollbar.css">
-        <link rel="stylesheet" href="{{url('public')}}/dist/css/theme.min.css">
-        <script src="{{url('public')}}/src/js/vendor/modernizr-2.8.3.min.js"></script>
-    </head>
-
-    <body>
-        <!--[if lt IE 8]>
-            <p class="browserupgrade">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> to improve your experience.</p>
-        <![endif]-->
-
-        <div class="auth-wrapper">
-            <div class="container-fluid h-100">
-                <div class="row flex-row h-100 bg-white">
-                    <div class="col-xl-8 col-lg-6 col-md-5 p-0 d-md-block d-lg-block d-sm-none d-none">
-                        <div class="lavalite-bg" style="background-image: url('{{url('public')}}/img/wai.jpeg')">
-                            <div class="lavalite-overlay"></div>
-                        </div>
-                    </div>
-                    <div class="col-xl-4 col-lg-6 col-md-7 my-auto p-0">
-                        <div class="authentication-form mx-auto">
-                            <h1>ADMIN BARU</h1>
-                            <p>Bergabunglah dengan kami hari ini! Hanya perlu beberapa langkah</p>
-                            <form action="../index.html">
-                                <div class="form-group">
-                                    <input type="text" class="form-control" placeholder="Email" required="">
-                                    <i class="ik ik-user"></i>
-                                </div>
-                                <div class="form-group">
-                                    <input type="password" class="form-control" placeholder="Password" required="">
-                                    <i class="ik ik-lock"></i>
-                                </div>
-                                <div class="form-group">
-                                    <input type="password" class="form-control" placeholder="Confirm Password" required="">
-                                    <i class="ik ik-eye-off"></i>
-                                </div>
-                                <div class="row">
-                                    <div class="col-12 text-left">
-                                        <label class="custom-control custom-checkbox">
-                                            <input type="checkbox" class="custom-control-input" id="item_checkbox" name="item_checkbox" value="option1">
-                                            <span class="custom-control-label">&nbsp;Saya Setuju <a href="#">Syarat dan ketentuan</a></span>
-                                        </label>
-                                    </div>
-                                </div>
-                                <div class="sign-btn text-center">
-                                    <button class="btn btn-theme">Buat Akun</button>
-                                </div>
-                            </form>
-                            <div class="register">
-                                <p>Sudah memiliki akun? <a href="{{url('login')}}">Login</a></p>
-                            </div>
-                        </div>
-                    </div>
-                </div>
+  <!-- Google Font: Source Sans Pro -->
+  <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
+  <!-- Font Awesome -->
+  <link rel="stylesheet" href="{{url('public')}}/plugins/fontawesome-free/css/all.min.css">
+  <!-- icheck bootstrap -->
+  <link rel="stylesheet" href="{{url('public')}}/plugins/icheck-bootstrap/icheck-bootstrap.min.css">
+  <!-- Theme style -->
+  <link rel="stylesheet" href="{{url('public')}}/dist/css/adminlte.min.css">
+</head>
+<body class="hold-transition register-page">
+<div class="register-box">
+  <div class="card card-outline card-primary">
+    <div class="card-header text-center">
+      <a href="{{url('beranda')}}" class="h1"><b>Admin</b>Vegefoods</a>
+    </div>
+    <div class="card-body">
+      <p class="login-box-msg">Silahkan Masuk Untuk Memulai Sistem Anda</p>
+      @include('template.utils.notif')
+      <form action="{{url('login')}}" method="post">
+     @csrf
+        <div class="input-group mb-3">
+          <input type="text" class="form-control" placeholder="Full name">
+          <div class="input-group-append">
+            <div class="input-group-text">
+              <span class="fas fa-user"></span>
             </div>
+          </div>
         </div>
-        
-        <script src="https://code.jquery.com/jquery-3.3.1.min.js"></script>
-        <script>window.jQuery || document.write('<script src="{{url('public')}}/src/js/vendor/jquery-3.3.1.min.js"><\/script>')</script>
-        <script src="{{url('public')}}/plugins/popper.js/dist/umd/popper.min.js"></script>
-        <script src="{{url('public')}}/plugins/bootstrap/dist/js/bootstrap.min.js"></script>
-        <script src="{{url('public')}}/plugins/perfect-scrollbar/dist/perfect-scrollbar.min.js"></script>
-        <script src="{{url('public')}}/plugins/screenfull/dist/screenfull.js"></script>
-        <script src="{{url('public')}}/dist/js/theme.js"></script>
-        <!-- Google Analytics: change UA-XXXXX-X to be your site's ID. -->
-        <script>
-            (function(b,o,i,l,e,r){b.GoogleAnalyticsObject=l;b[l]||(b[l]=
-            function(){(b[l].q=b[l].q||[]).push(arguments)});b[l].l=+new Date;
-            e=o.createElement(i);r=o.getElementsByTagName(i)[0];
-            e.src='https://www.google-analytics.com/analytics.js';
-            r.parentNode.insertBefore(e,r)}(window,document,'script','ga'));
-            ga('create','UA-XXXXX-X','auto');ga('send','pageview');
-        </script>
-    </body>
+        <div class="input-group mb-3">
+          <input type="email" class="form-control" placeholder="Email">
+          <div class="input-group-append">
+            <div class="input-group-text">
+              <span class="fas fa-envelope"></span>
+            </div>
+          </div>
+        </div>
+        <div class="input-group mb-3">
+          <input type="password" class="form-control" placeholder="Password">
+          <div class="input-group-append">
+            <div class="input-group-text">
+              <span class="fas fa-lock"></span>
+            </div>
+          </div>
+        </div>
+        <div class="row">
+          <div class="col-8">
+            <div class="icheck-primary">
+              <input type="checkbox" id="agreeTerms" name="terms" value="agree">
+              <label for="agreeTerms">
+               I agree to the <a href="#">terms</a>
+              </label>
+            </div>
+          </div>
+          <!-- /.col -->
+          <div class="col-4">
+            <button type="submit" class="btn btn-primary btn-block">Register</button>
+          </div>
+          <!-- /.col -->
+        </div>
+      </form>
+      <a href="login.html" class="text-center">I already have a membership</a>
+    </div>
+    <!-- /.form-box -->
+  </div><!-- /.card -->
+</div>
+<!-- /.register-box -->
+
+<!-- jQuery -->
+<script src="{{url('public')}}/plugins/jquery/jquery.min.js"></script>
+<!-- Bootstrap 4 -->
+<script src="{{url('public')}}/plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
+<!-- AdminLTE App -->
+<script src="{{url('public')}}/dist/js/adminlte.min.js"></script>
+</body>
 </html>
