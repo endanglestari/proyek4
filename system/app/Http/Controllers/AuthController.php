b89fb69e19806c376542f4f58f1095f868a6e463
endanglestari <71061298+endanglestari@users.noreply.github.com>
2021-01-07 22:45:12 +0700

    Halaman Login Pembeli Dan Penjual

--- system/app/Http/Controllers/AuthController.php
+++ system/app/Http/Controllers/AuthController.php
@@ -1,6 +1,8 @@
 <?php 
 
 namespace App\Http\Controllers;
+use App\Models\Pembeli;
+use App\Models\Penjual;
 use Auth;
 
 class AuthController extends Controller 
@@ -10,15 +12,57 @@ class AuthController extends Controller
 	}
 
 	function loginProcess(){
-		if(Auth::attempt(['email' => request('email'), 'password' => request('password')])){
-			return redirect('admin/beranda')->with('success', 'Login Anda Berhasil');
+		//if(Auth::attempt(['email' => request('email'), 'password' => request('password')])){
+			// $user = Auth::user();
+			// if($user->level == 1) return redirect('beranda/admin')->with('success', 'Login Anda Berhasil');
+			// if($user->level == 0) return redirect('beranda/pengguna')->with('success', 'Login Anda Berhasil');
+			//}else{
+			//return back()->with('danger', 'Login Anda Gagal, Silahkan Periksa Kembali email dan Password Anda. Terima Kasih');
+		//}
+
+		$email = request('email');
+		$user = Pembeli::where('email', $email)->first();
+		if($user){
+			$guard = 'pembeli';
 		}else{
-			return back()->with('danger', 'Login Anda Gagal, Silahkan Periksa Kembali email dan Password Anda. Terima Kasih');
+			$user = Penjual::where('email', $email)->first();
+			if($user){
+				$guard = 'penjual';
+			}else{
+				$guard = false;
+			}
 		}
+
+		if(!$guard) {
+			return back()->with('danger', 'Login Gagal, Email Tidak Ditemukan Di Database. Terima Kasih');
+	}else {
+			if(Auth::guard($guard)->attempt(['email' => request('email'), 'password' => request('password')])){
+				return redirect("beranda/$guard")->with('success', 'Login Anda Berhasil');
+			}else{
+				return back()->with('danger', 'Login Anda Gagal, Silahkan Periksa Kembali Email dan Password Anda. Terima Kasih');
+			}
+		}
+
+		//if(request('login_as') == 1){
+			//if(Auth::guard('pembeli')->attempt(['email' => request('email'), 'password' => request('password')])){
+				//return redirect('beranda/pembeli')->with('success', 'Login Anda Berhasil');
+			//}else{
+				//return back()->with('danger', 'Login Anda Gagal, Silahkan Periksa Kembali email dan Password Anda. Terima Kasih');
+				//}
+		//}else{
+			//if(Auth::guard('penjual')->attempt(['email' => request('email'), 'password' => request('password')])){
+				//return redirect('beranda/penjual')->with('success', 'Login Anda Berhasil');
+			//}else{
+				//return back()->with('danger', 'Login Anda Gagal, Silahkan Periksa Kembali email dan Password Anda. Terima Kasih');
+				//}
+
+		//}
 	}
 
 	function logout(){
 		Auth::logout();
+		Auth::guard('pembeli')->logout();
+		Auth::guard('penjual')->logout();
 		return redirect('beranda');
 	}
 
--- system/resources/views/beranda.blade.php
+++ system/resources/views/beranda.blade.php
@@ -2,7 +2,7 @@
 
 @section('content')
     <div class="col-md-12 ftco-animate text-center">
-        <h1 class="mb-2">Welcome To Admin</h1>
+        <h1 class="mb-2">Welcome To Pembeli</h1>
            <div class="slider-item"></div>
          <h4>Selamat Datang Di E-Commerce  Vegefoods</h4>
        <img src="{{url('public')}}/about.jpg">
